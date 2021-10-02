7d53a91f23dca70839aca94e8217b929d21e0622
Philipp Albrecht <palbrecht@mailbox.org>
2021-10-02 13:44:43 +0200

    Add constructor to Config

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents = fs::read_to_string(config.filename).expect("error reading the file");
-}
-
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        filename: args[2].clone(),
-    }
 }
 
 struct Config {
     query: String,
     filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
 }
