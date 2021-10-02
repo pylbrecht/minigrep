c4cbb66780f77ed5488b276ade5f0e65dfbb5050
Philipp Albrecht <palbrecht@mailbox.org>
2021-10-02 20:50:30 +0200

    Add unit tests covering Config::new()

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -22,5 +23,29 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_constructor() {
+        let args = vec![
+            "minigrep".to_string(),
+            "some query".to_string(),
+            "some/file.txt".to_string(),
+        ];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(&config.query, "some query");
+        assert_eq!(&config.filename, "some/file.txt");
+    }
+
+    #[test]
+    fn not_enough_args() {
+        let args = Vec::new();
+        let config = Config::new(&args);
+        assert_eq!(config.unwrap_err(), "not enough arguments");
     }
 }
